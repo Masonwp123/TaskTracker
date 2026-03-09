8b8264c27192430701b52e1c8ad24d563bb6d54d
Masonwp123 <masonparker868@gmail.com>
2026-03-09 09:50:23 -0400

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Button from 'primevue/button'
+</script>
+
+<template>
+  <div>
+    <Button label="hello" />
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import PrimeVue from 'primevue/config'
+import './style.css'
+import App from './App.vue'
+import Aura from '@primeuix/themes/aura'
+
+const app = createApp(App);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+})
+app.mount('#app')
